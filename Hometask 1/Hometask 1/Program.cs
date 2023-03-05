226d478f7c7a490fe81bc4a22ee14494104c87e7
Jahangirov97 <xaliq1919@gmail.com>
2023-03-05 13:33:42 +0400

    old project was wrong. That is why I have deleted. And new one.

---
+++ Hometask 1/Hometask 1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Hometask_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double mebleg = 10000;
+            
+            double dollarrate = 1.70d;
+            double eurorate = 1.80d;
+            double rublrate = 0.25d;
+
+
+            double dollar = mebleg / dollarrate;
+            double euro = mebleg / eurorate;
+            double rubl = mebleg / rublrate;
+
+            Console.WriteLine(" Məbləğ (AZN):");
+
+            Console.WriteLine(mebleg);
+
+
+
+            Console.WriteLine("ABŞ dolları (USD):");
+
+            Console.WriteLine(dollar);
+            //Console.WriteLine(dollar);
+
+            Console.WriteLine("Avro (EUR):");
+            Console.WriteLine(euro);
+
+            Console.WriteLine("Rusiya rublu (RUB):");
+            Console.Write(rubl);
+
+
+
+
+
+
+
+        }
+    }
+}
+        
+    
